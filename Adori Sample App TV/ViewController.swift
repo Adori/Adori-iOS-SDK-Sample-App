59bf7f21731ae8b9b22383e252a16510b989bc2c
Krishnaprasad Jagadish <53332323+krishnaparjanya@users.noreply.github.com>
2021-09-29 23:01:34 +0530

    Added tvOS support

---
+++ Adori Sample App TV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Adori Sample App TV
+//
+//  Created by Krishnaprasad Jagadish on 29/09/21.
+//
+
+import UIKit
+import AdoriAudioPlayerExtended
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
